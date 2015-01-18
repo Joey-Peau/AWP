1c422cc31c31ecf18ce814b11dab010659f18352
Joey Peau <joey.peau@gmail.com>
2015-01-19 00:24:06 +0200

    some improvement

--- Week 1/save.php
+++ Week 1/save.php
@@ -2,8 +2,16 @@
 include "../../config.php";
 
 // TODO: get the data from the form by using $_POST
+$data = array();
+$data['name'] = $_POST['name'];
+$data['description'] = $_POST['desc'];
+$data['email'] = $_POST['email'];
+$data['phone'] = $_POST['cell'];
+$date = $_POST['date'];
+$time = $_POST['time'];
 // this is how you convert the date from the form to SQL formatted date:
 // date ("Y-m-d H:i:s", strtotime(dataFromDateField.' '.dataFromTimeField));
+$data['sqlDate'] = date("Y-m-d H:i:s", strtotime($date . ' ' . $time));
 
 // this part was in dbConnect.php in last period:
 try {
@@ -19,15 +27,10 @@ try {
 }
 
 try {
-	var_dump($_POST);
-	$name = $_POST["name"];
-	$desc = $_POST["desc"];
-	$email = $_POST["email"];
-	$cell = $_POST["cell"];
-	$datetime = date("Y-m-d H:i:s", strtotime($_POST["date"] . " " . $_POST["time"]));
-	$res = $DBH->prepare("INSERT INTO calendar (eName, eDescription, pEmail, pPhone, eDate)
-		VALUES ('" . $name . "', '" . $desc . "', '" . $email . "', '" . $cell . "', '" . $datetime . "')");
-	$res->execute();
+	// TODO: insert the data from the form to database table 'calendar'
+	$STH = $DBH->prepare("INSERT INTO calendar (eName, eDescription, pEmail, pPhone, eDate) VALUES
+	(:name, :description, :email, :phone, :sqlDate);");
+	$STH->execute($data);
 
 } catch (PDOException $e) {
 	echo 'Something went wrong';
