dff548143343b4a5b655abea4fd208fe3463b3c3
Joey Peau <joey.peau@gmail.com>
2015-02-01 23:25:13 +0200

    Week 4 added

--- Week 2/save.php
+++ Week 2/save.php
@@ -1,30 +1,32 @@
 <?php
 include "../../config.php";
 
-// TODO: get the data from the form by using $_POST
-$data = array();
-$data['name'] = $_POST['name'];
-$data['description'] = $_POST['desc'];
-$data['email'] = $_POST['email'];
-$data['phone'] = $_POST['cell'];
-$date = $_POST['date'];
-$time = $_POST['time'];
-// this is how you convert the date from the form to SQL formatted date:
-// date ("Y-m-d H:i:s", strtotime(dataFromDateField.' '.dataFromTimeField));
-$data['sqlDate'] = date("Y-m-d H:i:s", strtotime($date . ' ' . $time));
+   // TODO: get the data from the form by using $_POST
+   $data = array();
+   $data['name'] = $_POST['name'];
+   $data['description'] = $_POST['desc'];
+   $data['email'] = $_POST['email'];
+   $data['phone'] = $_POST['cell'];
+   $date = $_POST['date'];
+   $time = $_POST['time'];
+   // this is how you convert the date from the form to SQL formatted date:
+   // date ("Y-m-d H:i:s", strtotime(dataFromDateField.' '.dataFromTimeField));
+   $data['sqlDate'] = date ("Y-m-d H:i:s", strtotime($date.' '.$time));;
+
 
 // this part was in dbConnect.php in last period:
 try {
 
 	$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-	$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	$DBH->query("SET NAMES 'UTF8';");
 
-} catch (PDOException $e) {
+}catch(PDOException $e) {
 
 	echo "Could not connect to database.";
 	file_put_contents('log.txt', $e->getMessage(), FILE_APPEND);
 }
+
 
 try {
 	// TODO: insert the data from the form to database table 'calendar'
@@ -34,6 +36,6 @@ try {
 
 } catch (PDOException $e) {
 	echo 'Something went wrong';
-	file_put_contents('log.txt', $e->getMessage() . "\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
+	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
 }
 ?>
